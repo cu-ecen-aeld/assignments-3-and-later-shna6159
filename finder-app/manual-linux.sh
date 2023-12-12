b99b2c18e172b429525576076a826126aecade94
shna6159 <shna6159@colorado.edu>
2023-12-11 22:03:12 -0700

    fix manual-linux.sh

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -2,7 +2,7 @@
 # Script outline to install and build kernel.
 # Author: Siddhant Jajoo.
 
-SET -x
+set -x
 set -e
 set -u
 
@@ -37,16 +37,15 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
 
     # TODO: Add your kernel build steps here
 
-   make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
 
     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
 
     make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
 
-
 fi
 
-cp ${OUTDIR}/linux-stable/arch/arm64/boot/Image #{OUTDIR}
+cp ${OUTDIR}/linux-stable/arch/arm64/boot/Image ${OUTDIR}
 
 echo "Adding the Image in outdir"
 
@@ -65,6 +64,7 @@ mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
 
+
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
@@ -72,6 +72,8 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
@@ -80,11 +82,13 @@ fi
 make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu-
 make CONFIG_PREFIX=${OUTDIR}/rootfs/ ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- install
 
+cd ${OUTDIR}/rootfs
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
-cd ${OUTDIR}/rootfs
+
 # TODO: Add library dependencies to rootfs
+
 SYSROOT_DIR=`aarch64-none-linux-gnu-gcc --print-sysroot`
 cp $SYSROOT_DIR/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
 
@@ -94,29 +98,30 @@ cp $SYSROOT_DIR/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
 
 cp $SYSROOT_DIR/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
 
-
-
 # TODO: Make device nodes
 cd ${OUTDIR}/rootfs
 sudo mknod -m 666 dev/null c 1 45
 
 # TODO: Clean and build the writer utility
-make -C $FINDER_APP/finder-app clean
-make -C $FIMDER_APP/finder-app CROSS_COMPILE=aarch64-none-linux-gnu-
+
+make -C $FINDER_APP_DIR clean
+make -C $FINDER_APP_DIR CROSS_COMPILE=aarch64-none-linux-gnu-
+
 # TODO: Copy the finder related scripts and executables to the /home directory
-cd ${OUTDIR}/rootfs
-cp $FINDER_APP/finder.sh ./home/
-cp $FINDER_APP/finder-test.sh ./home/
-cp $FINDER_APP/writer ./home/
-mkdir ./home/conf
-cp $FINDER_APP/conf/username.txt ./home/conf
-cp $FINDER_APP/conf/assignment.txt ./home/conf
-cp $FINDER_APP/autorun-qemu.sh ./home/
 # on the target rootfs
+cd ${OUTDIR}/rootfs
+cp $FINDER_APP_DIR/finder.sh ./home/
+cp $FINDER_APP_DIR/finder-test.sh ./home/
+cp $FINDER_APP_DIR/writer ./home/
+mkdir ./home/conf
+cp $FINDER_APP_DIR/conf/username.txt ./home/conf
+cp $FINDER_APP_DIR/conf/assignment.txt ./home/conf
+cp $FINDER_APP_DIR/autorun-qemu.sh ./home/
 
 # TODO: Chown the root directory
 cd ${OUTDIR}/rootfs
 sudo chown -R root:root .
+
 # TODO: Create initramfs.cpio.gz
 cd ${OUTDIR}/rootfs
 find .| cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio.gz
