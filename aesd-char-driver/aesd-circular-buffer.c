219c696301150deb60cece29bd33f5d26c64b7f2
shna6159 <shna6159@colorado.edu>
2024-02-28 00:25:59 -0700

    Assignment 7 pt 1 complete

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -29,9 +29,40 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+	// initialize variables for offsets
+	// keeps track of where we are in this continous block of data
+	size_t current_offset = 0;
+	size_t entry_size = 0;
+	// sets the entry ptr as zero intially
+	struct aesd_buffer_entry *entry = NULL;
+	
+	// checks if buffer is valid or not full
+	if (buffer == NULL) return NULL;
+	
+	// looping for entry
+	for (uint8_t i = 0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; i++) {
+		
+		// first location in entry to read from plus i iteration from a range of 10
+		uint8_t index = (buffer->out_offs + i) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+		
+		//entry defined above is now getting the entry from the array index that was defined in the CB struct
+		entry = &buffer->entry[index];
+		
+		//crucial bc ensures that buffer doesnt access entries where buffer isn't full yet
+		if (!buffer->full && buffer->in_offs) break;
+		
+		// size is number of bytes stored in buffptr
+		entry_size = entry->size;
+		
+		if (char_offset < (current_offset + entry_size)) {
+			
+			//this shows we found our entry point containing the offset
+			*entry_offset_byte_rtn = char_offset - current_offset;
+			return entry;
+		}
+		current_offset += entry_size;
+	}
+			
     return NULL;
 }
 
@@ -44,10 +75,32 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+	//check if buffer is full
+	//if not then add the entry 
+	//overwrite old entry then increment to new start or entry
+	
+	// full variable retrieved from struct in header file
+	if (buffer->full) {
+		
+		//advances out_offs to next location and wraps around if exceeds CB
+		buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+	}
+	
+	//copy the add_entry to the current in_offs position
+	buffer->entry[buffer->in_offs] = *add_entry;
+	
+	//advance the in_offs to the next position and wrapping if needed
+	// remember that in_offs is the current location in the entry structure where the next write should
+    //be stored.
+	buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+	
+	//checking if buffer is full, keep in mind that when in_offs = out_offs means that the
+	//in_offs has wrapped around to overlap with out_offs
+	if(buffer->in_offs == buffer->out_offs) {
+		buffer->full = true;
+	}
 }
+	
 
 /**
 * Initializes the circular buffer described by @param buffer to an empty struct
